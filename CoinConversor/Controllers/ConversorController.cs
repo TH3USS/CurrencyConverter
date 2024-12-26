d3eab0fda9d771ad0892b64d7001079ac68e9808
Matheus <matheusramossilva2003@gmail.com>
2024-12-26 19:42:15 -0300

    upload

---
+++ CoinConversor/Controllers/ConversorController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ConversorDeMoedas.Models;
+
+namespace ConversorDeMoedas.Controllers
+{
+    public class ConversorController : Controller
+    {
+        // Exibe o formulário para conversão
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Recebe os dados e faz a conversão
+        [HttpPost]
+        public IActionResult Converter(ConversorMoeda conversorMoeda)
+        {
+            if (ModelState.IsValid)
+            {
+                conversorMoeda.Converter();
+                return View("Resultado", conversorMoeda);
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ CoinConversor/Models/ConversorMoeda.cs
@@ -0,0 +1,33 @@
+﻿namespace ConversorDeMoedas.Models
+{
+    public class ConversorMoeda
+    {
+        public double Valor { get; set; }
+        public string MoedaOrigem { get; set; }
+        public string MoedaDestino { get; set; }
+        public double ValorConvertido { get; set; }
+
+        // Taxas de conversão fixas
+        private static readonly Dictionary<string, double> taxasDeConversao = new Dictionary<string, double>
+        {
+            { "USD", 1.0 }, // USD é a moeda base
+            { "BRL", 5.24 }, // Taxa fictícia de conversão do USD para BRL
+            { "EUR", 0.92 }, // Taxa fictícia de conversão do USD para EUR
+            { "GBP", 0.80 }  // Taxa fictícia de conversão do USD para GBP
+        };
+
+        public void Converter()
+        {
+            if (taxasDeConversao.ContainsKey(MoedaOrigem) && taxasDeConversao.ContainsKey(MoedaDestino))
+            {
+                double taxaOrigem = taxasDeConversao[MoedaOrigem];
+                double taxaDestino = taxasDeConversao[MoedaDestino];
+                ValorConvertido = Valor * (taxaDestino / taxaOrigem);
+            }
+            else
+            {
+                ValorConvertido = 0;
+            }
+        }
+    }
+}
